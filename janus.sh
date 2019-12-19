469174fcfb584d227f93f83c9de376bc3f830f5f
Gilles Biagomba <gbiago961@gmail.com>
2019-12-19 11:21:37 -0500

    Made some changes to improve

--- janus.sh
+++ janus.sh
@@ -2,7 +2,11 @@
 # Reference: https://www.owasp.org/index.php/Testing_for_HTTP_Verb_Tampering_(OTG-INPVAL-003)
 
 for webservmethod in GET POST PUT TRACE CONNECT OPTIONS PROPFIND; do
-	printf "$webservmethod " ;
-	printf "$webservmethod / HTTP/1.1\nHost: $1\n\n" | nc -q 1 $1 80 | grep "HTTP/1.1"
-	curl -X $webservmethod -d '<IMG SRC="javascript:alert('XSS');">' $1
+	echo "---------------------------------------------------------------------------------"
+	printf "Host: $1 \n"
+	printf "Testing HTTP $webservmethod Request: \n"
+	echo "---------------------------------------------------------------------------------"
+	curl -kL -X $webservmethod -d '<script>alert('document.cookie')</script>' $1
+	echo "---------------------------------------------------------------------------------"
+	echo
 done
