ef94a22bbf7f14854ef117f23debc7554bcf51a1
Gilles Biagomba <gbiago961@gmail.com>
2020-01-14 16:08:13 -0500

    Added a logo

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # OWASP-Janus
 This will test various HTTP Request types against a web server. This tool was named after the roman god of choices, cause you know you are testing for HTTP options ;).
 
+![alt tag](https://images.squarespace-cdn.com/content/v1/5b0c0ce212b13f38032df407/1546345063175-WFZGSLS422WOYRZB448U/ke17ZwdGBToddI8pDm48kOTw0z0GRa9jLSnKi1NEowhZw-zPPgdn4jUwVcJE1ZvWQUxwkmyExglNqGp0IvTJZUJFbgE-7XRK3dMEBRBhUpxPd_n6sohiW4hfnzFrq19wzNMyVyLKwqapB1v2VBra5epUCc2oVatrtGzV2lrE7v8/God-Janus-Image-xn903.jpg)
+
 ## Usage
 ```
 janus.sh www.example.com
--- janus.sh
+++ janus.sh
@@ -1,12 +1,13 @@
 #!/bin/env bash
 # Reference: https://www.owasp.org/index.php/Testing_for_HTTP_Verb_Tampering_(OTG-INPVAL-003)
 
-for webservmethod in GET POST PUT TRACE CONNECT OPTIONS PROPFIND; do
+for webservmethod in GET POST PUT TRACE CONNECT OPTIONS PROPFIND DELETE HEAD PATCH; do
 	echo "---------------------------------------------------------------------------------"
 	printf "Host: $1 \n"
 	printf "Testing HTTP $webservmethod Request: \n"
 	echo "---------------------------------------------------------------------------------"
-	curl -kL -X $webservmethod -d '<script>alert('document.cookie')</script>' $1
+	# curl -kL -X $webservmethod -d '<script>alert('document.cookie')</script>' $1
+	curl -kL -X $webservmethod $1
 	echo "---------------------------------------------------------------------------------"
 	echo
 done
