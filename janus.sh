3af648a6e6c75e61cf7e7221248322600125cb52
Gilles Biagomba <gbiago961@gmail.com>
2020-01-23 16:51:07 -0500

    Patched the reporting

--- janus.sh
+++ janus.sh
@@ -2,12 +2,12 @@
 # Reference: https://www.owasp.org/index.php/Testing_for_HTTP_Verb_Tampering_(OTG-INPVAL-003)
 
 for webservmethod in GET POST PUT TRACE CONNECT OPTIONS PROPFIND DELETE HEAD PATCH; do
-	echo "---------------------------------------------------------------------------------"
-	printf "Host: $1 \n"
-	printf "Testing HTTP $webservmethod Request: \n"
-	echo "---------------------------------------------------------------------------------"
+	echo "---------------------------------------------------------------------------------" | tee -a $(date +%m-%d-%Y)-janus_output.txt
+	printf "Host: $1 \n" | tee -a $(date +%m-%d-%Y)-janus_output.txt
+	printf "Testing HTTP $webservmethod Request: \n" | tee -a $(date +%m-%d-%Y)-janus_output.txt
+	echo "---------------------------------------------------------------------------------" | tee -a $(date +%m-%d-%Y)-janus_output.txt
 	# curl -kL -X $webservmethod -d '<script>alert('document.cookie')</script>' $1
 	curl -kLs -X $webservmethod $1 | tee -a $(date +%m-%d-%Y)-janus_output.txt
-	echo "---------------------------------------------------------------------------------"
-	echo
+	echo "---------------------------------------------------------------------------------" | tee -a $(date +%m-%d-%Y)-janus_output.txt
+	echo | tee -a $(date +%m-%d-%Y)-janus_output.txt
 done
