112e97f48a4b017a27812c4c9f0e093a2b9406a2
Gilles Biagomba <gbiago961@gmail.com>
2020-06-29 16:11:53 -0400

    Added more HTTP requests methods

--- janus.sh
+++ janus.sh
@@ -1,7 +1,8 @@
 #!/bin/env bash
 # Reference: https://www.owasp.org/index.php/Testing_for_HTTP_Verb_Tampering_(OTG-INPVAL-003)
+# Fork of https://github.com/gbiagomba/OWASP-Janus
 
-for webservmethod in GET POST PUT TRACE CONNECT OPTIONS PROPFIND DELETE HEAD PATCH; do
+for webservmethod in ACL BASELINE-CONTROL BCOPY BDELETE BMOVE BPROPFIND BPROPPATCH CHECKIN CHECKOUT CONNECT COPY DEBUG DELETE GET HEAD INDEX LABEL LOCK MERGE MKACTIVITY MKCOL MKWORKSPACE MOVE NOTIFY OPTIONS ORDERPATCH PATCH POLL POST PROPFIND PROPPATCH PUT REPORT RPC_IN_DATA RPC_OUT_DATA SEARCH SUBSCRIBE TRACE UNCHECKOUT UNLOCK UNSUBSCRIBE UPDATE VERSION-CONTROL X-MS-ENUMATTS; do
 	echo "---------------------------------------------------------------------------------" | tee -a $(date +%m-%d-%Y)-janus_output.txt
 	printf "Host: $1 \n" | tee -a $(date +%m-%d-%Y)-janus_output.txt
 	printf "Testing HTTP $webservmethod Request: \n" | tee -a $(date +%m-%d-%Y)-janus_output.txt
