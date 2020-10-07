94ccac0aa6fbe05722f87e57de7c9ef309b8bb16
Mad Gray Hatter <gbiagomba@users.noreply.github.com>
2020-10-07 17:15:29 -0400

    fixed output a bit

--- janus.sh
+++ janus.sh
@@ -7,13 +7,14 @@ if [ -z $1 ]; then
 	exit
 fi
 
+{
 for webservmethod in ACL BASELINE-CONTROL BCOPY BDELETE BMOVE BPROPFIND BPROPPATCH CHECKIN CHECKOUT CONNECT COPY DEBUG DELETE GET HEAD INDEX LABEL LOCK MERGE MKACTIVITY MKCOL MKWORKSPACE MOVE NOTIFY OPTIONS ORDERPATCH PATCH POLL POST PROPFIND PROPPATCH PUT REPORT RPC_IN_DATA RPC_OUT_DATA SEARCH SUBSCRIBE TRACE UNCHECKOUT UNLOCK UNSUBSCRIBE UPDATE VERSION-CONTROL X-MS-ENUMATTS; do
-	echo "---------------------------------------------------------------------------------" | tee -a $(date +%m-%d-%Y)-janus_output.txt
-	printf "Host: $1 \n" | tee -a $(date +%m-%d-%Y)-janus_output.txt
-	printf "Testing HTTP $webservmethod Request: \n" | tee -a $(date +%m-%d-%Y)-janus_output.txt
-	echo "---------------------------------------------------------------------------------" | tee -a $(date +%m-%d-%Y)-janus_output.txt
+	echo "---------------------------------------------------------------------------------"
+	printf "Host: $1 \n"
+	printf "Testing HTTP $webservmethod Request: \n"
+	echo "---------------------------------------------------------------------------------"
 	# curl -kL -X $webservmethod -d '<script>alert('document.cookie')</script>' $1
-	curl -ikLs --max-time 10 -X $webservmethod $1 | tee -a $(date +%m-%d-%Y)-janus_output.txt
-	echo "---------------------------------------------------------------------------------" | tee -a $(date +%m-%d-%Y)-janus_output.txt
-	echo | tee -a $(date +%m-%d-%Y)-janus_output.txt
+	curl -ikLs --max-time 10 -X $webservmethod $1
+	echo
 done
+} | tee -a $(date +%m-%d-%Y)-janus_output.txt
