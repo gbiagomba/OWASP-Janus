2c463eb4ca8a754ce2c76c8f78a1eec66171f9fe
Mad Gray Hatter <gbiagomba@users.noreply.github.com>
2020-10-07 17:10:05 -0400

    Update janus.sh
    adding a check to make sure the input isnt empty

--- janus.sh
+++ janus.sh
@@ -2,6 +2,11 @@
 # Reference: https://www.owasp.org/index.php/Testing_for_HTTP_Verb_Tampering_(OTG-INPVAL-003)
 # Fork of https://github.com/gbiagomba/OWASP-Janus
 
+if [ -z $1 ]; then
+	echo "You did not specify a target (e.g., www.example.com)"
+	exit
+fi
+
 for webservmethod in ACL BASELINE-CONTROL BCOPY BDELETE BMOVE BPROPFIND BPROPPATCH CHECKIN CHECKOUT CONNECT COPY DEBUG DELETE GET HEAD INDEX LABEL LOCK MERGE MKACTIVITY MKCOL MKWORKSPACE MOVE NOTIFY OPTIONS ORDERPATCH PATCH POLL POST PROPFIND PROPPATCH PUT REPORT RPC_IN_DATA RPC_OUT_DATA SEARCH SUBSCRIBE TRACE UNCHECKOUT UNLOCK UNSUBSCRIBE UPDATE VERSION-CONTROL X-MS-ENUMATTS; do
 	echo "---------------------------------------------------------------------------------" | tee -a $(date +%m-%d-%Y)-janus_output.txt
 	printf "Host: $1 \n" | tee -a $(date +%m-%d-%Y)-janus_output.txt
