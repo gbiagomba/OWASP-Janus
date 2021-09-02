98abcd8d13c23ad22ee9eca5a8866f03f2718432
D4rth R3v4n <gbiagomba@users.noreply.github.com>
2021-09-02 18:09:40 -0400

    Update janus.sh

--- janus.sh
+++ janus.sh
@@ -11,7 +11,6 @@ fi
 for webservmethod in ACL BASELINE-CONTROL BCOPY BDELETE BMOVE BPROPFIND BPROPPATCH CHECKIN CHECKOUT CONNECT COPY DEBUG DELETE GET HEAD INDEX LABEL LOCK MERGE MKACTIVITY MKCOL MKWORKSPACE MOVE NOTIFY OPTIONS ORDERPATCH PATCH POLL POST PROPFIND PROPPATCH PUT REPORT RPC_IN_DATA RPC_OUT_DATA SEARCH SUBSCRIBE TRACE UNCHECKOUT UNLOCK UNSUBSCRIBE UPDATE VERSION-CONTROL X-MS-ENUMATTS; do
 	SiteStatus=$(curl -o /dev/null -k --silent --max-time 3 -X $webservmethod --write-out "%{http_code} $1\n" "$1" | cut -d " " -f 1)
 	if [ "$SiteStatus" != "304" ] || [ "$SiteStatus" != "405" ] || [ "$SiteStatus" != "302" ]; then
-		echo "---------------------------------------------------------------------------------"
 		printf "HTTP $webservmethod Request Method - $1 - "
 		curl -IkLs --max-time 3 -X $webservmethod $1 | grep -i "HTTP/1.1"
 		echo
