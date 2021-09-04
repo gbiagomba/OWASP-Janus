1b0f7806be1743916eab6e02046542462804d74d
Mad Gray Hatter <gbiagomba@users.noreply.github.com>
2021-09-04 18:36:36 -0400

    Fixed multithread issue

--- janus.sh
+++ janus.sh
@@ -2,14 +2,17 @@
 # Reference: https://www.owasp.org/index.php/Testing_for_HTTP_Verb_Tampering_(OTG-INPVAL-003)
 # reference for parallel: https://stackoverflow.com/questions/23814360/gnu-parallel-and-bash-functions-how-to-run-the-simple-example-from-the-manual
 
-if [ -z $1 ]; then
-	echo "You did not specify a target (e.g., www.example.com)"
+if ! hash curl 2> /dev/null; then
+	echo "Please install curl - https://curl.se/download.html & try again"
 	exit
 fi
 
 # initiallizing variables
+outputfile="$PWD/janus_output-$current_time"
 current_time=$(date "+%Y.%m.%d-%H.%M.%S")
 curl_cmd="curl -IkLs --max-time 3 -X"
+tmpdir=`mktemp -d`
+declare -i min=0
 declare -i c=10
 
 # Menu & flags
@@ -76,21 +79,30 @@ fi
 # Functions
 function main
 {
-	local_url=$1
+	local local_url=$1
 	for webservmethod in ${httpMethods[*]}; do
 		SiteStatus=$(curl -o /dev/null -k --silent --max-time 3 -X $webservmethod --write-out "%{http_code} $1\n" "$1" | cut -d " " -f 1)
 		if [ "$SiteStatus" != "304" ] && [ "$SiteStatus" != "302" ] && [ "$SiteStatus" != "403" ] && [ "$SiteStatus" != "405" ] && [ "$SiteStatus" != "000" ] && [ ! -z $SiteStatus ]; then
-			printf "HTTP $webservmethod Request Method - $local_url - "
-			$curl_cmd $webservmethod $local_url | grep -i "HTTP/1."
+			printf "HTTP $webservmethod Request Method - $local_url - $($curl_cmd $webservmethod $local_url | grep -i "HTTP/" | tr "\n" " ")" | tee -a $tmpdir/`echo $local_url | tr "/" "_" | tr ":" "_"`-janus_output-$current_time.txt
+			echo
 		fi
 	done
 }
 
-# Launching attack
-{
-	if [ ! -z $url ]; then
-		main $url
-	elif [ ! -z $targetfile ]; then
-		for i in `cat $targetfile`; do main $i; done
-	fi
-} | tee -a $outputfile
+# Launching main
+if [ ! -z $url ]; then
+	main $url
+elif [ ! -z $targetfile ]; then
+	for i in `cat $targetfile`; do
+		 main $i &
+		let "min+=1"
+		if (( $min == $c ));then
+			while pgrep -x curl > /dev/null; do sleep 10; done; min=0
+		fi
+	done
+fi
+
+# Combining output & generating cvs
+cat  $tmpdir/*.txt | sort -u | tee -a $outputfile.out
+echo "http_verb,url,proto_version,status_code,status_response" >> $outputfile.csv
+cat $outputfile.out | cut -d " " -f 2,6,8-20 | tr " " "," >> $outputfile.csv
